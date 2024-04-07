ba9510af865c569ea28668be1d96a06ed81e886a
caresle <carabesle@gmail.com>
2024-04-07 13:23:42 -0600

    Add basic login card

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,10 @@
+import LoginCard from "@/components/login/LoginCard";
 import React from "react";
 
 export default function Login() {
-	return <div>Login</div>;
+	return (
+		<div className="h-screen flex justify-center items-center">
+			<LoginCard />
+		</div>
+	);
 }
--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Separator } from "../ui/separator";
 
 export default function LoginCard() {
-	return <div>LoginCard</div>;
+	return (
+		<Card className="w-3/12 mx-auto h-fit shadow-lg">
+			<CardHeader>
+				<CardTitle className="text-center">Welcome to Rello</CardTitle>
+			</CardHeader>
+
+			<CardContent>
+				<Input placeholder="Username" className="mb-4" />
+				<Input placeholder="Password" type="password" />
+			</CardContent>
+
+			<CardFooter className="flex flex-col gap-1">
+				<Button className="w-full">Login</Button>
+				<Button className="w-full" variant={"outline"}>
+					Sign Up
+				</Button>
+
+				{/* AUTH with social platforms */}
+				<Separator className="my-2" />
+				<span>Or login with</span>
+			</CardFooter>
+		</Card>
+	);
 }
