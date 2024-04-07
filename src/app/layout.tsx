993716811607baa4d8b8a30396660538fdafe923
caresle <carabesle@gmail.com>
2024-04-07 13:12:05 -0600

    Add editor config file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Rello",
-  description: "",
+	title: "Rello",
+	description: "",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`bg-gray-100 ${inter.className}`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`bg-gray-100 ${inter.className}`}>{children}</body>
+		</html>
+	);
 }
