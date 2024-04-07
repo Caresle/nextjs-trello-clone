079351651f8eb4989a54a341f99b5bc90bcd3676
caresle <carabesle@gmail.com>
2024-04-07 13:43:52 -0600

    Add social login buttons

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -9,12 +9,23 @@ import {
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Separator } from "../ui/separator";
+import Icon from "../shared/Icon";
+import Image from "next/image";
 
 export default function LoginCard() {
 	return (
-		<Card className="w-3/12 mx-auto h-fit shadow-lg">
+		<Card className="w-3/12 mx-auto h-fit shadow-xl">
 			<CardHeader>
 				<CardTitle className="text-center">Welcome to Rello</CardTitle>
+				<div className="w-full flex justify-center">
+					<Image
+						className="rounded-full"
+						src="/logo.jpeg"
+						width={128}
+						height={128}
+						alt=""
+					/>
+				</div>
 			</CardHeader>
 
 			<CardContent>
@@ -22,15 +33,28 @@ export default function LoginCard() {
 				<Input placeholder="Password" type="password" />
 			</CardContent>
 
-			<CardFooter className="flex flex-col gap-1">
-				<Button className="w-full">Login</Button>
+			<CardFooter className="flex flex-col gap-2">
+				<Button className="w-full">
+					<Icon.Actions.Login className="text-2xl mr-2" />
+					Login
+				</Button>
 				<Button className="w-full" variant={"outline"}>
+					<Icon.Actions.LogOut className="text-2xl mr-2" />
 					Sign Up
 				</Button>
 
 				{/* AUTH with social platforms */}
 				<Separator className="my-2" />
 				<span>Or login with</span>
+
+				<Button className="w-full">
+					<Icon.Social.Github className="text-xl mr-2" />
+					GitHub
+				</Button>
+				<Button className="w-full" variant={"outline"}>
+					<Icon.Social.Google className="text-xl mr-2" />
+					Google
+				</Button>
 			</CardFooter>
 		</Card>
 	);
---
+++ src/components/shared/Icon.tsx
@@ -0,0 +1,14 @@
+import { IoLogInOutline, IoLogOutOutline } from "react-icons/io5";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+const Icon = {
+	Actions: {
+		Login: IoLogInOutline,
+		LogOut: IoLogOutOutline,
+	},
+	Social: {
+		Github: FaGithub,
+		Google: FaGoogle,
+	},
+};
+
+export default Icon;
